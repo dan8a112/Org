470b06ca63300ca9aef0e16f23df0bc5eefbdb32
dan8a112 <danyochoa112@gmail.com>
2023-09-05 14:07:40 -0600

    Aula 01 Finalizada y avance de aula 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header.js';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo
         </p>
         <a
           className="App-link"
